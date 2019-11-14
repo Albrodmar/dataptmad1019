e24151d91099231dec4945e92b23e7858377d478
Alberto Rodriguez <alroma@gmail.com>
2019-11-14 22:12:03 +0100

    Stuck on exercise 16

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,79 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a=np.random.random((2,3,5))
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b= np.ones((5,2,3))
 
 #6. Print b.
-
+print('Exercise 6')
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+#ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3)
+# They have different size. Not possible in python to multiply two arrays with different size
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#ValueError: operands could not be broadcast together with shapes (2,3,5) (5,2,3)
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+print('Exercise 9')
+c = b.reshape(2,3,5)
+print(c.shape, c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+print('Exercise 10')
+d= (a+c)
+# it works because a and c have the same size
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
+print("Exercise 11")
+print(a)
+print(d)
 
+#d array is the same as a but every element has a +1 value.
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+print('Exercise 12')
+e = a*c
 
 #13. Does e equal to a? Why or why not?
-
-
+print('Exercise 13')
+print(e == a)
+# All the elements are equal so both arrays are equal
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
+print('Exercise 14')
+d_max=np.max(d)
+print(f'd_max: {d_max}')
+d_min=np.min(d)
+print(f'd_min: {d_min}')
+d_mean=np.mean(d)
+print(f'd_mean: {d_mean}')
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+print('Exercise 15')
+f = np.empty([2,3,5])
 
 
 """
@@ -74,7 +85,10 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+print('Exercise 16')
 
+for x in np.nditer(d)
+    if d_min < x > d_mean:
 
 
 
